3d68e3466111d4ed747b9af26b17e18600715b63
taylor <taylor.mccants@uzh.ch>
2020-05-23 09:42:25 +0200

    used AppBar for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,34 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import logo from "../views/slides2video_logo.png";
 
-export const HeaderContainer = styled.div`
-  display: inline-block;
-  width: 100%;
-  background-color: #1a0dab;
-  color: white;
-  height: 4em;
-`;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        textAlign: 'Left',
+        margin: 5,
+        minHeight: 30,
+    },
+}));
 
-export const LogoContainer = styled.div`
-    text-align: left;
-    margin-left: 1em;
-    margin-top: 0.5em;
-    margin-bottom: 0.5em;
-`;
+export default function Header() {
+    const classes = useStyles();
 
-export const MenuContainer = styled.div`
-    text-align: right;
-`;
-
-class Header extends Component {
-
-    render() {
-        return (
-            <HeaderContainer>
-                <LogoContainer>
-                    <img src={logo} width={'15%'}/>
-                </LogoContainer>
-                <MenuContainer>
-                </MenuContainer>
-            </HeaderContainer>
-        )
-    }
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <h2 className={classes.title}>slides2video</h2>
+                    <PlayArrowIcon style={{fontSize: 30}}/>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
-
-export default Header;
