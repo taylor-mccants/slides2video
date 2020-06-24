415ac68cc05be24ee989e767ffc8cf3e8b5585d0
taylor <taylor.mccants@uzh.ch>
2020-06-24 12:55:04 +0200

    linear loading and breakpoints using @media

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import DropBox from "./DropBox";
 import VerticalStepper from "./Stepper";
-import {Button, CircularProgress, TextField, withStyles} from '@material-ui/core';
+import {Button, TextField} from '@material-ui/core';
 import CompletionDialog from "./CompletionDialog";
 import Grid from "@material-ui/core/Grid";
 import SnackbarAlert from "./Alert";
 import isEmail from 'validator/lib/isEmail';
 import {submitSlides} from "../api"
+import LinearProgress from "@material-ui/core/LinearProgress";
 
 function HowTo(props) {
 
@@ -86,36 +87,39 @@ function HowTo(props) {
 
 
     return (
-        <Grid container style={{padding: '40px', maxWidth: "800px", display: 'flex', flexDirection: 'column'}}
-              id='HowToContent'>
-            <h2 style={{marginBottom: '32px', marginTop: '-5px', textAlign: 'left'}}>Easily convert your PowerPoint
-                slides into lecture videos!</h2>
-            <VerticalStepper activeStep={activeStep}/>
-            <DropBox onFileSelect={handleFileSelect} files={files} showAlert={showAlert}/>
-            <div style={{display: 'flex'}}>
-                <Grid item lg={8} md={8} sm={8} style={{display: 'flex'}}>
-                    <TextField style={{width: '90%'}} label='Email' variant='outlined'
-                               key="email" value={emailAddress} onKeyPress={onEnterPress} autoComplete="email"
-                               onChange={(e) => {
-                                   handleInputChange("emailAddress", e.target.value);
-                               }}
-                    />
-                </Grid>
-                <Grid item lg={4} md={4} sm={4} style={{display: 'flex', flexDirection: 'row-reverse'}}>
-                    <Button disabled={!files.length > 0 || !isEmail(emailAddress)}
-                            variant="contained"
-                            color='primary'
-                            onClick={handleSubmit}
-                            style={{minWidth: '56px', fontWeight: 'bold'}}>
-
-                        {loading ? <CircularProgress color={'white'}/> : 'Create Video'}
-                    </Button>
-                </Grid>
-            </div>
-            <CompletionDialog dialogOpen={dialogOpen} handleCloseDialog={handleCloseDialog} email={emailAddress}/>
-            <SnackbarAlert open={alertOpen} severity={alertSeverity}
-                           message={alertMessage} handleClose={handleCloseAlert}/>
-        </Grid>
+        <div>
+            <Grid container style={{padding: '40px', maxWidth: "800px", display: 'flex', flexDirection: 'column'}}
+                  id='HowToContent'>
+                <h2 style={{marginBottom: '32px', marginTop: '-5px', textAlign: 'left'}}>Easily convert your PowerPoint
+                    slides into lecture videos!</h2>
+                <VerticalStepper activeStep={activeStep}/>
+                <DropBox onFileSelect={handleFileSelect} files={files} showAlert={showAlert}/>
+                <div style={{display: 'flex'}}>
+                    <Grid item lg={8} md={8} sm={8} style={{display: 'flex'}}>
+                        <TextField style={{width: '90%'}} label='Email' variant='outlined'
+                                   key="email" value={emailAddress} onKeyPress={onEnterPress} autoComplete="email"
+                                   onChange={(e) => {
+                                       handleInputChange("emailAddress", e.target.value);
+                                   }}
+                        />
+                    </Grid>
+                    <Grid item lg={4} md={4} sm={4} style={{display: 'flex', flexDirection: 'row-reverse'}}>
+                        <Button disabled={!files.length > 0 || !isEmail(emailAddress)}
+                                variant="contained"
+                                color='primary'
+                                onClick={handleSubmit}
+                                style={{minWidth: '56px', fontWeight: 'bold'}}>
+                            Create Video
+                            {/*loading ? <CircularProgress color={'white'}/> : 'Create Video'*/}
+                        </Button>
+                    </Grid>
+                </div>
+                <CompletionDialog dialogOpen={dialogOpen} handleCloseDialog={handleCloseDialog} email={emailAddress}/>
+                <SnackbarAlert open={alertOpen} severity={alertSeverity}
+                               message={alertMessage} handleClose={handleCloseAlert}/>
+            </Grid>
+          {loading ? <div><LinearProgress/></div> : <div/>}
+        </div>
     );
 }
 
